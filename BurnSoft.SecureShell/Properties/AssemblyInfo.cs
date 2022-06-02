204bd84c129c4d8491f76275ea9b9345f318c23a
BurnSoft <bs.github@burnsoft.net>
2022-06-02 11:50:02 -0400

    updated libraries and postbuild batch file

--- BurnSoft.SecureShell/Properties/AssemblyInfo.cs
+++ BurnSoft.SecureShell/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.SecureShell")]
-[assembly: AssemblyCopyright("Copyright © BurnSoft 2019-2021")]
+[assembly: AssemblyCopyright("Copyright © BurnSoft 2019-2022")]
 [assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
